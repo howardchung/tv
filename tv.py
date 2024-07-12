2c8e1a87c3f8c0a245ca613eea1a7ef473c199b1
Howard Chung <howardzchung@gmail.com>
2024-07-12 15:53:43 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -5,22 +5,22 @@ import os
 import signal
 import atexit
 import json
+import sys
 
 # king-hd (nbc) or komo (abc)
-#http://127.0.0.1:9981/play/ticket/stream/channel/98d79ac2602abd0678317916fb2bf044?title=4.1%20%3A%20KOMO
-#://127.0.0.1:9981/play/ticket/stream/channel/0fb23aab6bc3f953589495c112fcff37?title=5.1%20%3A%20KING-
 stream: subprocess.Popen = None
+adapter = sys.argv[1]
+url = 'https://howardchung.github.io/tv/adapter' + adapter + '.html'
+atexit.register(kill)
 
 def launch(id):
-    return subprocess.Popen('dvbv5-zap --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -preset ultrafast -x264-params keyint=60 -b:v 4M -c:a aac -ac 1 -f flv rtmp://5.161.147.222/live/abc', shell=True)
+    return subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -preset ultrafast -x264-params keyint=60 -b:v 4M -c:a aac -ac 1 -f flv rtmp://5.161.147.222/live/' + adapter, shell=True)
 
 def kill():
     print('kill group')
     os.killpg(os.getpgid(stream.pid), signal.SIGTERM)
 
-atexit.register(kill)
-
-x = requests.get('https://howardchung.github.io/tv/selected.html')
+x = requests.get(url)
 channel = x.text.strip()
 stream = launch(channel)
 # Repeat every 3 seconds
@@ -28,7 +28,7 @@ stream = launch(channel)
 while True:
     time.sleep(3)
     try:
-        x = requests.get('https://howardchung.github.io/tv/selected.html')
+        x = requests.get(url)
         # new = json.loads(x.text)['title']
         new = x.text.strip()
         # print(new, channel)
@@ -37,9 +37,7 @@ while True:
         if new != channel:
             channel = new
             if stream != None:
-                print('kill process')
-                os.system("killall -9 ffmpeg");
-                os.system("killall -9 dvbv5-zap");
+                kill()
             stream = launch(new)
         # If process crashes, restart it
         if stream.poll() != None:
