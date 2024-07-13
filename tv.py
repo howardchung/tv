a357ebba8cd0bacc6998613caa5882945fee10c2
Howard Chung <howardzchung@gmail.com>
2024-07-13 00:12:47 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -18,7 +18,7 @@ def kill():
 atexit.register(kill)
 
 def launch(id):
-    return subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -preset veryfast -x264-params keyint=60 -b:v 3M -c:a aac -ac 1 -f flv rtmp://5.161.147.222/live/' + adapter, shell=True)
+    return subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -preset superfast -x264-params keyint=60 -b:v 3M -c:a aac -ac 1 -f flv rtmp://5.161.147.222/live/' + adapter, shell=True)
 
 x = requests.get(url)
 channel = x.text.strip()
