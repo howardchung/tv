7d9126e51b0dd152822065c9704c050a5b0b44e3
Howard Chung <howardzchung@gmail.com>
2024-07-14 00:47:39 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -24,7 +24,7 @@ def launch(id):
     global stream
     if not id:
         return
-    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -vf scale=-1:720 -r 30 -crf ' + crf + ' -preset ' + preset + ' -x264-params keyint=60 -c:a aac -ac 1 -f flv rtmp://5.161.147.222/live/' + adapter, shell=True, preexec_fn=os.setsid)
+    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -vf scale=-1:720 -crf ' + crf + ' -preset ' + preset + ' -x264-params keyint=60 -c:a aac -ac 1 -f flv rtmp://5.161.147.222/live/' + adapter, shell=True, preexec_fn=os.setsid)
 
 atexit.register(kill)
 x = requests.get(url)
