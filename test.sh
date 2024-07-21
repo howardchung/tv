14950c5f5616b682adb901dfcaa2daadd64510c2
Howard Chung <howardzchung@gmail.com>
2024-07-21 04:42:10 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-dvbv5-zap --adapter=1 --input-format=ZAP -c channels.conf -o - "king-hd" | nc -l -p 5000 5.161.147.222
+dvbv5-zap --adapter=1 --input-format=ZAP -c channels.conf -o - "king-hd" | nc 5.161.147.222 5000
