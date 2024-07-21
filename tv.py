91ad66182d5bcd2d59fe0fd46cfa5ee58050e1d0
Howard Chung <howardzchung@gmail.com>
2024-07-21 16:42:56 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -13,7 +13,7 @@ try:
 except:
     adapter = "0"
 url = "https://backend.watchparty.me/roomData/tender-squirrel-reproduce"
-preset = "veryfast"
+preset = "superfast"
 
 def kill():
     global stream
@@ -27,7 +27,7 @@ def launch(id):
         return
     #-vf scale=-1:720
     #-vf fps=30
-    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -tune zerolatency -c:v libx264 -x264-params keyint=120 -crf 25 -preset ' + preset + ' -c:a aac -ac 1 -f flv rtmp://5.161.147.222/live/' + id, shell=True, preexec_fn=os.setsid)
+    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -tune zerolatency -c:v libx264 -x264-params keyint=120 -preset ' + preset + ' -c:a aac -ac 1 -f flv rtmp://5.161.147.222/live/' + id, shell=True, preexec_fn=os.setsid)
 
 atexit.register(kill)
 x = requests.get(url)
