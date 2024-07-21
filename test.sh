efa3a14ba42e73177a9dce9a734e6bf8e181b212
Howard Chung <howardzchung@gmail.com>
2024-07-21 03:57:04 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-dvbv5-zap --adapter=1 --input-format=ZAP -c channels.conf -o - "king-hd" | ffmpeg -err_detect ignore_err -i pipe: -c:v copy -c:a copy -f mpegts rtmp://5.161.147.222/live/mpeg
+dvbv5-zap --adapter=1 --input-format=ZAP -c channels.conf -o - "king-hd" | ffmpeg -err_detect ignore_err -i pipe: -c:v copy -c:a copy -f mpegts rtmp://5.161.147.222/mpeg/king-hd
