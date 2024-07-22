3da5848a4f6d93583a78d10b4fc0eac4dc14b037
Howard Chung <howardzchung@gmail.com>
2024-07-21 18:05:08 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ dvbv5-zap --adapter=1 --input-format=ZAP -c channels.conf -o - "king-hd" | nc -u
 #receive
 #ffmpeg -listen 1 -i tcp://0.0.0.0:5000 -c:v libx264 -preset veryfast -crf 23 -c:a aac -ac 1 -f flv rtmp://localhost:1935/live/king-hd
 #ffmpeg -listen 1 -i tcp://0.0.0.0:5000 -c:v libx264 -preset veryfast -c:a aac -ac 1 -hls_flags delete_segments -f hls /var/www/html/stream/king-hd.m3u8
-ffmpeg -err_detect ignore_err -i udp://localhost:5000 -c:v libx264 -preset fast -c:a aac -ac 2 -hls_flags delete_segments -hls_start_number_source epoch -f hls /var/www/html/stream/tv.m3u8
+ffmpeg -err_detect ignore_err -i udp://localhost:5000 -c:v libx264 -preset faster -c:a aac -ac 2 -hls_flags delete_segments -hls_start_number_source epoch -f hls /var/www/html/stream/tv.m3u8
