c528a69a57242a699842bf0163212fdc8c833859
Howard Chung <howardzchung@gmail.com>
2024-07-22 02:47:22 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 #send
 dvbv5-zap --adapter=1 --input-format=ZAP -c channels.conf -o - "king-hd" | nc -u 5.161.147.222 5000
-#dvbv5-zap --adapter=1 --input-format=ZAP -c channels.conf -o - "king-hd" | ffmpeg -i pipe: -c:v libx264 -preset superfast -c:a aac -ac 1 -f flv rtmp://5.161.147.222:1935/live/king-hd
+#dvbv5-zap --adapter=1 --input-format=ZAP -c channels.conf -o - "king-hd" | ffmpeg -i pipe: -c:v libx264 -preset superfast -c:a aac -ac 1 -f flv rtmp://5.161.147.222:1935/transcode/king-hd
 
 #receive
 #ffmpeg -listen 1 -i tcp://0.0.0.0:5000 -c:v libx264 -preset veryfast -c:a aac -ac 1 -f flv rtmp://localhost:1935/live/king-hd
