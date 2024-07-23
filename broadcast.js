80aee3ec4c45693bed59146dda9143dffba3f475
Howard Chung <howardzchung@gmail.com>
2024-07-23 14:08:50 -0700

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -1,13 +1,20 @@
 const http = require("http");
 const { spawn } = require('child_process');
-const ffmpeg = spawn('ffmpeg', ['-i', 'rtmp://localhost/live/tv', '-c:v', 'copy', '-c:a', 'copy', '-f', 'mpegts', '-']);
-
 const sockets = new Map();
+
+const ffmpeg = spawn('ffmpeg', ['-i', 'rtmp://localhost/live/tv', '-c:v', 'copy', '-c:a', 'copy', '-f', 'mpegts', '-']);
+ffmpeg.stdout.on('data', (data) => {
+  for (let res of sockets.values()) {
+      console.log('wrote %s bytes to %s sockets', data.length, sockets.size);
+      res.write(data);
+  }
+});
+
 const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     const rand = Math.random();
     sockets.set(rand, res);
-    res.on('finished', () => {
+    res.on('end', () => {
       console.log('deleting socket %s', rand);
       sockets.delete(rand);
     });
@@ -16,13 +23,4 @@ const host = '0.0.0.0';
 const port = 8081;
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
-});
-server.on('connection', (socket) => {
-
-});
-ffmpeg.stdout.on('data', (data) => {
-  for (let res of sockets.values()) {
-      console.log('wrote %s bytes to %s sockets', data.length, sockets.size);
-      res.write(data);
-  }
 });
