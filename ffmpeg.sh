9f3dd9dcd9bcdce50d7f64c8f6356cb3cad27c60
Howard Chung <howardzchung@gmail.com>
2024-07-24 16:20:01 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -4,7 +4,7 @@
 #mpegts stream over http for mpegts.js players
 # nginx proxies incoming requests on port 80 to broadcast server on 8081
 ffmpeg -err_detect ignore_err -listen 1 -i tcp://0.0.0.0:5000 -c:v libx265 -preset faster -c:a aac -ac 2 -f mpegts - | ffmpeg -err_detect ignore_err -i pipe: \
--c copy -f hls -hls_time 2 -hls_list_size 1000 -hls_start_number_source epoch -hls_flags delete_segments -hls_segment_type fmp4 /tmp/hls/tv.m3u8 \
+-c copy -tag:v hvc1 -f hls -hls_time 2 -hls_list_size 1000 -hls_start_number_source epoch -hls_flags delete_segments -hls_segment_type fmp4 /tmp/hls/tv.m3u8 \
 -c copy -f mpegts - | node /root/tv/broadcast.js
 
 
