363dd8881209ef77728345c631e50ac0b6240c3a
Howard Chung <howardzchung@gmail.com>
2024-07-24 16:31:08 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -14,6 +14,6 @@ ffmpeg -err_detect ignore_err -listen 1 -i tcp://0.0.0.0:5000 -c:v libx265 -pres
 #-f mp4 -movflags frag_keyframe+empty_moov
 #-c:v libx264 -preset fast -crf 23 -x264-params keyint=60
 #-c:v libsvtav1 -g 60 -preset 11
-#-c:v libx265 -preset faster
+#-c:v libx265 -preset faster -x265-params keyint=60
 # could also transcode to multiple resolutions and save to separate hls streams
 #-vf scale=-1:320
