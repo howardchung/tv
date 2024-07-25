1e149bbe0e6904bb3b336d280b623f4727452825
Howard Chung <howardzchung@gmail.com>
2024-07-25 04:09:07 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -3,7 +3,7 @@
 #HLS for hls.js players, serve segments with nginx
 #mpegts stream over http for mpegts.js players
 # nginx proxies incoming requests on port 80 to broadcast server on 8081
-ffmpeg -err_detect ignore_err -listen 1 -i tcp://0.0.0.0:5000 -c:v libx264 -preset fast -g 60 -keyint_min 60 -c:a aac -ac 2 -c:s mov_text -f mp4 -movflags empty_moov+frag_keyframe - | ffmpeg -err_detect ignore_err -i pipe: \
+ffmpeg -err_detect ignore_err -listen 1 -i tcp://0.0.0.0:5000 -c:v libx265 -preset veryfast -x265-params "keyint=60:min-keyint=60" -c:a aac -ac 2 -c:s mov_text -f mp4 -movflags empty_moov+frag_keyframe - | ffmpeg -err_detect ignore_err -i pipe: \
 -c copy -f hls -hls_time 2 -hls_list_size 2000 -hls_start_number_source epoch -hls_flags delete_segments -hls_segment_type fmp4 /tmp/hls/tv.m3u8 \
 -c copy -f dash -adaptation_sets "id=0,streams=v id=1,streams=a" -window_size 2000 -frag_duration 2 /tmp/dash/tv.mpd \
 -c copy -f mpegts - | node /root/tv/broadcast.js
