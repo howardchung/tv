4ddf0985aee2036db26f917ef035aec17e41cef7
Howard Chung <howardzchung@gmail.com>
2024-07-26 14:59:17 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -13,7 +13,7 @@ nc -l 5000 \
 | ffmpeg -err_detect ignore_err -f mp4 -i pipe: \
 -c copy -f hls -hls_time 2 -hls_list_size 2000 -hls_start_number_source epoch -hls_flags delete_segments -hls_segment_type fmp4 /var/www/hls/tv.m3u8 \
 -c copy -f dash -adaptation_sets "id=0,streams=v id=1,streams=a" -window_size 2000 -frag_duration 2 /var/www/dash/tv.mpd \
--c copy -f ismv - \
+-c copy -f mp4 -movflags empty_moov+frag_keyframe+default_base_moof - \
 | node /root/tv/broadcast.js 8082
 
 
