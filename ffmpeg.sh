b5bfb89947045a7c3e02400b90e201d258ba4bf1
Howard Chung <howardzchung@gmail.com>
2024-07-26 22:58:34 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -7,11 +7,12 @@
 nc -l 5000 \
 | node /root/tv/broadcast.js 8080 \
 | ffmpeg -err_detect ignore_err -f mpegts -i pipe: -c:v libx264 -preset veryfast -g 60 -keyint_min 60 -c:a aac -ac 2 -f mpegts - \
+| node /root/tv/broadcast.js 8081 \
 | ffmpeg -err_detect ignore_err -f mpegts -i pipe: \
 -c copy -f hls -hls_time 2 -hls_list_size 3000 -hls_start_number_source epoch -hls_flags delete_segments /var/www/hls/tv.m3u8 \
--c copy -f dash -dash_segment_type mp4 -window_size 3000 -frag_duration 2 /var/www/dash/tv.mpd \
--c copy -f mpegts - \
-| node /root/tv/broadcast.js 8081 > /dev/null
+-c copy -f mp4 -movflags frag_keyframe+empty_moov - \
+| ffmpeg -err_detect ignore_err -f mp4 -i pipe: \
+-c copy -f dash -dash_segment_type mp4 -window_size 3000 -frag_duration 2 /var/www/dash/tv.mpd
 
 # As of July 2024 mpegts doesn't support av1 (planned in future)
 # apple hls spec says only fmp4 is supported for x265 or av1 content
