f44196fde470d940e7a0ffeaa43dd83f54fc4051
Howard Chung <howardzchung@gmail.com>
2024-08-03 13:17:31 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -8,8 +8,8 @@ nc -l 5000 \
 | node /root/tv/broadcast.js 8080 \
 | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -x264-params "keyint=60:scenecut=0" -preset veryfast -c:a aac -ac 2 -c:s mov_text -r 30 -f mp4 -movflags frag_keyframe+empty_moov - \
 | ffmpeg -err_detect ignore_err -i pipe: \
--c copy -f hls -hls_time 2 -hls_list_size 5400 -hls_flags delete_segments+append_list -hls_segment_type fmp4 /var/www/hls/tv.m3u8 \
--c copy -f dash -seg_duration 2 -window_size 300  /var/www/dash/tv.mpd \
+-c copy -f hls -hls_time 2 -hls_list_size 4500 -hls_flags delete_segments+append_list -hls_segment_type fmp4 /var/www/hls/tv.m3u8 \
+-c copy -f dash -seg_duration 2 -window_size 150  /var/www/dash/tv.mpd \
 -c copy -f mpegts - \
 | node /root/tv/broadcast.js 8081 > /dev/null
 
