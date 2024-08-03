5f116df5c5a466fa39ca7dd86e1c948be35ff049
Howard Chung <howardzchung@gmail.com>
2024-08-03 14:23:45 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -6,9 +6,9 @@
 
 nc -l 5000 \
 | node /root/tv/broadcast.js 8080 \
-| ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -x264-params "keyint=60:scenecut=0" -preset veryfast -c:a aac -ac 2 -c:s mov_text -r 30 -f mp4 -movflags frag_keyframe+empty_moov - \
+| ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -x264-params "keyint=60:scenecut=0" -preset veryfast -crf 25 -c:a aac -ac 2 -c:s mov_text -r 30 -f mp4 -movflags frag_keyframe+empty_moov - \
 | ffmpeg -err_detect ignore_err -i pipe: \
--c copy -f hls -hls_time 2 -hls_list_size 4500 -hls_flags delete_segments+append_list -hls_segment_type fmp4 /var/www/hls/tv.m3u8 \
+-c copy -f hls -hls_time 2 -hls_list_size 5400 -hls_flags delete_segments+append_list -hls_segment_type fmp4 /var/www/hls/tv.m3u8 \
 -c copy -f dash -seg_duration 2 -window_size 150  /var/www/dash/tv.mpd \
 -c copy -f mpegts - \
 | node /root/tv/broadcast.js 8081 > /dev/null
@@ -20,7 +20,7 @@ nc -l 5000 \
 # when using DASH subtitles need to be declared in mpd: descriptor=<Accessibility schemeIdUri=\"urn:scte:dash:cc:cea-608:2015\" value=\"CC1=eng\"/>
 # output fmp4 stream: -f mp4 -movflags frag_keyframe+empty_moov
 # tested working presets for 3 amd cores (cpx series)
-#-c:v libx264 -preset fast -x264-params "keyint=60:scenecut=0"
+#-c:v libx264 -preset fast -crf 25 -x264-params "keyint=60:scenecut=0"
 #-c:v libsvtav1 -g 60 -preset 11
 #-c:v libx265 -preset veryfast -x265-params "keyint=60:min-keyint=60"
 # could also transcode to multiple resolutions and save to separate hls streams
