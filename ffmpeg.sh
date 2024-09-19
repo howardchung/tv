e14126602a23b9f90c29bd3791976427880b020a
Howard Chung <howardzchung@gmail.com>
2024-09-18 23:59:39 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -4,9 +4,9 @@
 # mpegts stream over http for mpegts.js players
 # nginx proxies incoming requests on port 80 to broadcast server on 8081
 
+# | node /root/tv/broadcast.js 8080 \
+# | ffmpeg -err_detect ignore_err -f mpegts -i pipe: -c:v libsvtav1 -g 60 -preset 11 -c:a aac -ac 2 -c:s mov_text -r 30 -f mp4 -movflags frag_keyframe+empty_moov - \
 nc -N -l 5000 \
-| node /root/tv/broadcast.js 8080 \
-| ffmpeg -err_detect ignore_err -f mpegts -i pipe: -c:v libsvtav1 -g 60 -preset 11 -c:a aac -ac 2 -c:s mov_text -r 30 -f mp4 -movflags frag_keyframe+empty_moov - \
 | ffmpeg -err_detect ignore_err -i pipe: \
 -c copy -f hls -hls_time 2 -hls_list_size 7200 -hls_flags delete_segments -hls_segment_type fmp4 /var/www/hls/tv.m3u8 \
 -c copy -f dash -seg_duration 2 -window_size 150  /var/www/dash/tv.mpd \
