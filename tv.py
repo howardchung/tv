840219c9d47f5fa578721f838164207e3c5033ce
Howard Chung <howardzchung@gmail.com>
2024-09-20 03:19:36 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -30,7 +30,7 @@ def launch(id):
     stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_error -i pipe: -c:v libx264 -preset ' + preset + ' -c:a aac -ac 2 -r 30 -f mpegts tcp://5.78.115.83:5000', shell=True, preexec_fn=os.setsid)
     # Need to set env var since we're using old drivers (not iHD)
     os.environ["LIBVA_DRIVER_NAME"] = "i965"
-    #stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_error -hwaccel vaapi -vaapi_device /dev/dri/renderD128 -i pipe: -vf \'format=nv12,hwupload\' -c:v h264_vaapi -qp 26 -c:a aac -ac 2 -r 30 -f mp4 -movflags frag_keyframe+empty_moov - | nc 5.78.115.83 5000', shell=True, preexec_fn=os.setsid)
+    #stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_error -hwaccel vaapi -vaapi_device /dev/dri/renderD128 -i pipe: -vf \'format=nv12,hwupload\' -c:v h264_vaapi -qp 26 -c:a aac -ac 2 -r 30 -f mp4 -movflags frag_keyframe+empty_moov tcp://5.78.115.83:5000', shell=True, preexec_fn=os.setsid)
 
 def getChannel():
     #return requests.get(url).json()["video"].split("/")[-1].split(".")[0]
