45ea6ddebda06f42313c4c17c753a72a43d6e178
Howard Chung <howardzchung@gmail.com>
2024-10-14 21:13:19 -0700

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -6,7 +6,7 @@
 
 # libsvtav1 -g 60 -preset 11
 # | node /root/tv/broadcast.js 8080 \
-socat -T 15 TCP-LISTEN:5000 - \
+socat -T 30 TCP-LISTEN:5000 - \
 | ffmpeg -err_detect ignore_err -i pipe: -f mp4 -movflags frag_keyframe+empty_moov - \
 | ffmpeg -err_detect ignore_err -i pipe: \
 -c copy -f hls -hls_time 2 -hls_list_size 7200 -hls_flags delete_segments -hls_segment_type fmp4 /var/www/hls/tv.m3u8 \
