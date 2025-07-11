4d1227d91c99bf3de4e83fbf383b4d7c6c2f3576
Howard Chung <howardzchung@gmail.com>
2025-07-11 01:51:19 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -35,7 +35,7 @@ def launch(id):
     #-f mp4 -movflags frag_keyframe+empty_moov
     # Need to set env var since we're using old drivers (not iHD)
     os.environ["LIBVA_DRIVER_NAME"] = "i965"
-    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libsvtav1 -g 60 -preset 13 -c:a aac -ac 2 -r 30 -f flv rtmp://5.78.115.83:5000', shell=True, preexec_fn=os.setsid)
+    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -preset superfast -x264-params "keyint=60:scenecut=0" -c:a aac -ac 2 -r 30 -f flv rtmp://5.78.115.83:5000', shell=True, preexec_fn=os.setsid)
 
 def getChannel():
     #return requests.get(url).json()["video"].split("/")[-1].split(".")[0]
