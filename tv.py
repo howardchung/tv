875b186b5831be07206150783af522c795aa5381
Howard Chung <howardzchung@gmail.com>
2025-07-11 00:45:38 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -32,9 +32,10 @@ def launch(id):
     #-c:v libsvtav1 -g 60 -preset 13
     #-c:v copy
     #-f mpegts
+    #-f mp4 -movflags frag_keyframe+empty_moov
     # Need to set env var since we're using old drivers (not iHD)
     os.environ["LIBVA_DRIVER_NAME"] = "i965"
-    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libsvtav1 -g 60 -preset 13 -c:a aac -ac 2 -r 30 -f ogg tcp://5.78.115.83:5000', shell=True, preexec_fn=os.setsid)
+    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libsvtav1 -g 60 -preset 13 -c:a aac -ac 2 -r 30 -f mp4 -movflags frag_keyframe+empty_moov tcp://5.78.115.83:5000', shell=True, preexec_fn=os.setsid)
 
 def getChannel():
     #return requests.get(url).json()["video"].split("/")[-1].split(".")[0]
