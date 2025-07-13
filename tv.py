c954cc301eb000bf4728b2ffd2bee0b7c87263d1
Howard Chung <howardzchung@gmail.com>
2025-07-12 23:18:22 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -37,7 +37,7 @@ def launch(id):
     #-f flv rtmp://5.78.115.83:5000
     # Need to set env var since we're using old drivers (not iHD)
     os.environ["LIBVA_DRIVER_NAME"] = "i965"
-    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -preset veryfast -x264-params "keyint=60:scenecut=0" -b:v 3M -c:a aac -ac 2 -r 30 -f hls -hls_time 2 -hls_list_size 3600 -hls_flags delete_segments -hls_segment_type fmp4 ~/backblaze/tv.m3u8', shell=True, preexec_fn=os.setsid)
+    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -err_detect ignore_err -i pipe: -c:v libx264 -preset veryfast -x264-params "keyint=60:scenecut=0" -b:v 3M -c:a aac -ac 2 -r 30 -f hls -hls_time 2 -hls_list_size 3600 -hls_flags delete_segments -hls_segment_type fmp4 /backblaze/tv.m3u8', shell=True, preexec_fn=os.setsid)
 
 def getChannel():
     #return requests.get(url).json()["video"].split("/")[-1].split(".")[0]
