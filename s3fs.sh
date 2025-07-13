c49860a1d9f35b3de90ce7631fa606702fcfd1fb
Howard Chung <howardzchung@gmail.com>
2025-07-12 22:54:55 -0700

    Create s3fs.sh

---
+++ s3fs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo '0xxxxxxxxxxxxxx7:KxxxxxxxxxxxxxxxxxQ' > /etc/passwd-s3fs
+sudo chmod 640 /etc/passwd-s3fs
+sudo mkdir -p ~/backblaze
+sudo s3fs mybucket ~/backblaze -o passwd_file=/etc/passwd-s3fs -o url=https://s3.us-east-005.backblazeb2.com
