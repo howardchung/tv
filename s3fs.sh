22f37e54f973f20e3afd9968099fdb058d20e3c4
Howard Chung <howardzchung@gmail.com>
2025-07-12 23:00:14 -0700

    Update s3fs.sh

--- s3fs.sh
+++ s3fs.sh
@@ -3,4 +3,4 @@
 echo '0xxxxxxxxxxxxxx7:KxxxxxxxxxxxxxxxxxQ' > /etc/passwd-s3fs
 sudo chmod 640 /etc/passwd-s3fs
 sudo mkdir -p ~/backblaze
-sudo s3fs mybucket ~/backblaze -o passwd_file=/etc/passwd-s3fs -o url=https://s3.us-east-005.backblazeb2.com
+sudo s3fs watchparty-hls ~/backblaze -o passwd_file=/etc/passwd-s3fs -o url=https://s3.us-east-005.backblazeb2.com
