1c50a373227950262aa43363d3ee125aeefc27d5
Howard Chung <howardzchung@gmail.com>
2025-07-16 01:46:40 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -31,19 +31,23 @@ def launch(idAndName):
     if not id:
         return
     #-vf scale=-1:720
-    #-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v h264_vaapi -sei -a53_cc -g 60
-    #-c:v libx264 -preset veryfast -x264-params "keyint=60:scenecut=0"
-    #-c:v libx265 -preset superfast -x265-params "keyint=60:min-keyint=60"
-    #-c:v libsvtav1 -g 60 -preset 12
-    #-c:v copy
-    #-f mpegts
-    #-f mp4 -movflags frag_keyframe+empty_moov
-    #-f hls -hls_time 10 -hls_list_size 360 -hls_flags delete_segments /mnt/watchparty-hls/tv.m3u8
-    #-tag:v av01 -tag:a mp4a -f dash -seg_duration 4 -window_size 900  /mnt/watchparty-hls/tv.mpd
-    #-f flv rtmp://5.78.115.83:5000
+    encode = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v h264_vaapi -sei -a53_cc -g 60 -b:v 4M'
+    encode2 = '-c:v libx264 -preset superfast -x264-params "keyint=60:scenecut=0" -b:v 3M'
+    encode3 = '-c:v libx265 -preset superfast -x265-params "keyint=60:min-keyint=60"'
+    encode4 = '-c:v libsvtav1 -g 60 -preset 12'
+    encode5 = '-c:v copy'
+    
+    container_hls = '-f hls -hls_time 8 -hls_list_size 5400 -hls_flags delete_segments'
+    container_dash = '-tag:v av01 -tag:a mp4a -f dash -seg_duration 8 -window_size 5400'
+    container3 = '-f flv'
+    container4 = '-f mp4 -movflags frag_keyframe+empty_moov'
+    
+    outname_hls = '/mnt/watchparty-hls/' + name + '.m3u8'
+    outname_dash = '/mnt/watchparty-hls' + name + '.mpd'
+    outname3 = 'rtmp://5.78.115.83:5000'
     # Need to set env var since we're using old drivers (not iHD)
     #os.environ["LIBVA_DRIVER_NAME"] = "i965"
-    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -i pipe: -c:v libx264 -preset superfast -x264-params "keyint=60:scenecut=0" -b:v 3M -c:a aac -ac 2 -r 30 -f nut - | ffmpeg -i pipe: -c copy -f hls -hls_time 8 -hls_list_size 5400 -hls_flags delete_segments /mnt/watchparty-hls/' + name + '.m3u8', shell=True, preexec_fn=os.setsid)
+    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -i pipe: ' + encode2 + ' -c:a aac -ac 2 -r 30 -f nut - | ffmpeg -i pipe: -c copy ' + container_hls + ' ' + outname_hls, shell=True, preexec_fn=os.setsid)
 
 def getChannelAndName():
     data = requests.get(url).json()["video"]
