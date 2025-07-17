8bab08fead65dfa484ed5f22c7ddc5400c96fa79
Howard Chung <howardzchung@gmail.com>
2025-07-17 10:31:19 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -35,8 +35,8 @@ def launch(id):
     encode4 = '-c:v libsvtav1 -g 60 -preset 12'
     encode5 = '-c:v copy'
     
-    container_hls = '-f hls -hls_time 6 -hls_list_size 3600 -hls_flags delete_segments'
-    container_dash = '-tag:v av01 -tag:a mp4a -f dash -seg_duration 6 -window_size 3600'
+    container_hls = '-f hls -hls_time 6 -hls_list_size 2400 -hls_flags delete_segments'
+    container_dash = '-tag:v av01 -tag:a mp4a -f dash -seg_duration 6 -window_size 2400'
     container3 = '-f flv'
     container4 = '-f mp4 -movflags frag_keyframe+empty_moov'
     
@@ -45,7 +45,7 @@ def launch(id):
     outname3 = 'rtmp://5.78.115.83:5000'
     # Need to set env var since we're using old drivers (not iHD)
     os.environ["LIBVA_DRIVER_NAME"] = "i965"
-    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -i pipe: ' + encode2 + ' -c:a aac -ac 2 -r 30 -f mpegts - | ffmpeg -i pipe: -c copy ' + container_hls + ' ' + outname_hls, shell=True, preexec_fn=os.setsid)
+    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | ffmpeg -i pipe: ' + encode2 + ' -c:a aac -ac 2 -r 30 ' + container_hls + ' ' + outname_hls, shell=True, preexec_fn=os.setsid)
 
 def getChannel():
     data = requests.get(url).json()["video"]
