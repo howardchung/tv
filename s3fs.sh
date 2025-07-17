8e750a53b42dfd92cc205e2c3f043ecd8d426d6d
Howard Chung <howardzchung@gmail.com>
2025-07-17 10:52:18 -0700

    Update s3fs.sh

--- s3fs.sh
+++ s3fs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo '0xxxxxxxxxxxxxx7:KxxxxxxxxxxxxxxxxxQ' > /etc/passwd-s3fs
-sudo chmod 640 /etc/passwd-s3fs
-mkdir -p /mnt
-s3fs watchparty-hls /mnt -o passwd_file=/etc/passwd-s3fs -o url=https://s3.us-east-005.backblazeb2.com
+# echo '0xxxxxxxxxxxxxx7:KxxxxxxxxxxxxxxxxxQ' > /etc/passwd-s3fs
+# sudo chmod 640 /etc/passwd-s3fs
+# mkdir -p /mnt
+# s3fs watchparty-hls /mnt -o passwd_file=/etc/passwd-s3fs -o url=https://s3.us-east-005.backblazeb2.com
 
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 rclone config
-rclone mount backblaze:/ /mnt
+rclone mount -v --disable copy backblaze:/ /mnt
