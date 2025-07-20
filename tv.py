b8d9fd407b218149b2efd6f35c9b79895a81dbf6
Howard Chung <howardzchung@gmail.com>
2025-07-20 01:01:23 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -32,13 +32,13 @@ def launch(id):
     #-vf scale=-1:720
     encode1 = '-c:v copy'
     encode2 = '-c:v libx264 -preset veryfast -x264-params "keyint=60:scenecut=0"' #-crf 28
-    encode3 = '-c:v libx265 -preset veryfast -x265-params "keyint=60:min-keyint=60"'
+    encode3 = '-c:v libx265 -preset superfast -x265-params "keyint=60:min-keyint=60"'
     encode4 = '-c:v libsvtav1 -g 60 -preset 12'
     encode5 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v h264_vaapi -sei -a53_cc -g 60 -qp 28'
     encode6 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v hevc_vaapi -sei -a53_cc -g 60 -qp 28'
     encode7 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v av1_vaapi -sei -a53_cc -g 60 -qp 28'
 
-    container_hls = '-f hls -hls_time 6 -hls_list_size 1200 -hls_flags delete_segments'
+    container_hls = '-f hls -hls_time 6 -hls_list_size 1200 -hls_flags delete_segments -hls_segment_type fmp4'
     container_dash = '-f dash -seg_duration 6 -window_size 1200' #-tag:v av01 -tag:a mp4a 
     container_flv = '-f flv'
     container_fmp4 = '-f mp4 -movflags frag_keyframe+empty_moov'
