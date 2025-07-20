7b6915e701c4449d06294379184ee9543ee9bba9
Howard Chung <howardzchung@gmail.com>
2025-07-20 00:02:31 -0700

    Update and rename s3fs.sh to rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -5,6 +5,6 @@
 # mkdir -p /mnt
 # s3fs watchparty-hls /mnt -o passwd_file=/etc/passwd-s3fs -o url=https://s3.us-east-005.backblazeb2.com
 
-sudo -v ; curl https://rclone.org/install.sh | sudo bash
-rclone config
+#sudo -v ; curl https://rclone.org/install.sh | sudo bash
+#rclone config
 rclone mount -v --vfs-cache-mode=writes --vfs-write-back=2s backblaze:/ /mnt
