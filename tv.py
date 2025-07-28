0c726a0e55e43b542ddb838a857428a0df642aa6
Howard Chung <howardzchung@gmail.com>
2025-07-28 01:17:24 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -37,7 +37,7 @@ def launch(id):
     encode6 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v hevc_vaapi -sei -a53_cc -g 60 -qp 26'
     encode7 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v av1_vaapi -sei -a53_cc -g 60 -qp 26'
 
-    container_hls = '-f hls -hls_time 4 -hls_list_size 3600 -hls_flags delete_segments append_list' #-hls_segment_type fmp4
+    container_hls = '-f hls -hls_time 4 -hls_list_size 3600 -hls_flags delete_segments+append_list' #-hls_segment_type fmp4
     container_dash = '-f dash -seg_duration 4 -window_size 900' #-tag:v av01 -tag:a mp4a 
     container_flv = '-f flv'
     container_fmp4 = '-f mp4 -movflags frag_keyframe+empty_moov'
