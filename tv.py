6938d5d9a70277cf196763cf628cf53769ee42d0
Howard Chung <howardzchung@gmail.com>
2025-08-04 21:14:49 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -30,14 +30,14 @@ def launch(id):
         return
     #-vf scale=-1:720
     encode1 = '-c:v copy'
-    encode2 = '-c:v libx264 -preset veryfast -x264-params "keyint=144:scenecut=0" -crf 26'
-    encode3 = '-c:v libx265 -preset veryfast -x265-params "keyint=144"'
-    encode4 = '-c:v libsvtav1 -g 144 -preset 9'
-    encode5 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v h264_vaapi -sei -a53_cc -g 144 -qp 26'
-    encode6 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v hevc_vaapi -sei -a53_cc -g 144 -qp 26'
-    encode7 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v av1_vaapi -sei -a53_cc -g 144 -qp 26'
+    encode2 = '-c:v libx264 -preset veryfast -x264-params "keyint=150:scenecut=0" -crf 26'
+    encode3 = '-c:v libx265 -preset veryfast -x265-params "keyint=150"'
+    encode4 = '-c:v libsvtav1 -g 150 -preset 9'
+    encode5 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v h264_vaapi -sei -a53_cc -g 150 -qp 26'
+    encode6 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v hevc_vaapi -sei -a53_cc -g 150 -qp 26'
+    encode7 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v av1_vaapi -sei -a53_cc -g 150 -qp 26'
 
-    container_hls = '-f hls -hls_time 4.8 -hls_list_size 2000 -hls_flags delete_segments+append_list' # -hls_segment_type fmp4
+    container_hls = '-f hls -hls_time 5 -hls_list_size 2000 -hls_flags delete_segments+append_list' # -hls_segment_type fmp4
     container_dash = '-f dash -seg_duration 4 -window_size 900' #-tag:v av01 -tag:a mp4a 
     container_flv = '-f flv'
     container_fmp4 = '-f mp4 -movflags frag_keyframe+empty_moov'
