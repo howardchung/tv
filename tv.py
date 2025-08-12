981cc0e15464f43dd20ac4a05c694f17877dc768
Howard Chung <howardzchung@gmail.com>
2025-08-12 12:53:31 -0700

    Update tv.py

--- tv.py
+++ tv.py
@@ -37,7 +37,7 @@ def launch(id):
     encode6 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v hevc_vaapi -sei -a53_cc -g 150 -qp 26'
     encode7 = '-vaapi_device /dev/dri/renderD128 -vf \'format=nv12,hwupload\' -c:v av1_vaapi -sei -a53_cc -g 150 -qp 26'
 
-    container_hls = '-f hls -hls_time 5 -hls_list_size 2000 -hls_flags delete_segments+append_list' # -hls_segment_type fmp4
+    container_hls = '-f hls -hls_time 5 -hls_list_size 2000 -hls_flags append_list' # -hls_segment_type fmp4
     container_dash = '-f dash -seg_duration 4 -window_size 900' #-tag:v av01 -tag:a mp4a 
     container_flv = '-f flv'
     container_fmp4 = '-f mp4 -movflags frag_keyframe+empty_moov'
@@ -51,14 +51,35 @@ def launch(id):
         encode = encode5
     #subprocess.Popen('rm /mnt/watchparty-hls/' + curr + '*', shell=True)
     #subprocess.Popen('rm /mnt/watchparty-hls/init.mp4', shell=True)
-    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | node broadcast.js 8080 | ffmpeg -i pipe: ' + encode + ' -c:a aac -ac 2 -r 30 ' + container_hls + ' ' + outname_hls, shell=True, preexec_fn=os.setsid)
+    stream = subprocess.Popen('dvbv5-zap --adapter=' + adapter + ' --input-format=ZAP -c channels.conf -o - "' + id + '" | node broadcast.js 8080 | ffmpeg -i pipe: ' + encode + ' -c:a aac -ac 2 -r 30 -f nut - | ffmpeg -i pipe: -c copy ' + container_hls + ' ' + outname_hls, shell=True, preexec_fn=os.setsid)
 
 def getChannel():
     data = requests.get(url).json()["video"]
     if data:
         return data.split(".m3u8")[0].split("/")[-1]
     return ''
-    
+
+# function to perform delete operation based on condition
+def check_and_delete(folder):
+    # folder is the name of the folder in which we have to perform the delete operation
+    folder = "/mnt/watchparty-hls"
+   # loop to check all files one by one 
+   # os.walk returns 3 things: current path, files in the current path, and folders in the current path 
+   for (root,dirs,files) in os.walk(folder, topdown=True):
+       for f in files:
+           # temp variable to store path of the file 
+           file_path = os.path.join(root,f)
+           # get the timestamp, when the file was modified 
+           timestamp_of_file_modified = os.path.getmtime(file_path)
+           # convert timestamp to datetime
+           modification_date = datetime.datetime.fromtimestamp(timestamp_of_file_modified)
+           # find the number of hours when the file was modified
+           diff = datetime.datetime.now() - modification_date
+           if diff.total_seconds() > 6 * 3600:
+               # remove file 
+               os.remove(file_path)
+               print(f" Delete : {f}")
+               
 atexit.register(kill)
 curr = getChannel()
 launch(curr)
